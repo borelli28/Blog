75da357415858b258a7709083d1cd4542dd857f7
borelli28 <borelliarmando28@gmail.com>
2024-08-19 18:57:17 -0400

    Added password hasher

--- user-management/src/models.rs
+++ user-management/src/models.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
-
+use dotenvy::dotenv;
+use std::env;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
@@ -13,4 +14,17 @@ pub struct User {
 pub struct LoginCredentials {
     pub username: String,
     pub password: String,
+}
+
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Argon2,
+};
+
+pub async fn hash(password: &[u8]) -> String {
+    let salt = SaltString::generate(&mut OsRng);
+    Argon2::default()
+        .hash_password(password, &salt)
+        .expect("Unable to hash password.")
+        .to_string()
 }
