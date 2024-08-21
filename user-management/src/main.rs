f5082df880991afa17bb516228ddc542afc81b57
borelli28 <borelliarmando28@gmail.com>
2024-08-21 15:05:56 -0400

    Added index method

--- user-management/src/main.rs
+++ user-management/src/main.rs
@@ -1,6 +1,21 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 pub mod models;
 pub mod db;
 
-fn main() {
-    println!("Hello, world!");
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/User")
+                .route("/", web::get().to(index))
+        )
+    })
+    .bind(("127.0.0.1", 1234))?
+    .run()
+    .await
 }
