344d5adc072ce342964073cdcdae69d3934b6928
borelli <borelliarmando28@gmail.com>
2024-08-24 22:21:18 -0400

    Fixed errors in csrf module

--- csrf/src/token.rs
+++ csrf/src/token.rs
@@ -1,8 +1,8 @@
+use actix_web::{web, HttpResponse};
 use actix_session::Session;
 use rand::Rng;
 use rand::distributions::Alphanumeric;
 use actix_web::cookie::{Cookie, SameSite};
-use actix_web::{HttpRequest};
 
 const CSRF_TOKEN_KEY: &str = "csrf_token";
 
@@ -14,33 +14,30 @@ pub fn generate_csrf_token() -> String {
         .collect()
 }
 
-pub fn verify_csrf_token(session: &Session, token: &str) -> bool {
-    match session.get::<String>(CSRF_TOKEN_KEY) {
+pub async fn get_csrf_token(session: Session) -> HttpResponse {
+    let csrf_token = generate_csrf_token();
+    session.insert(CSRF_TOKEN_KEY, csrf_token.clone()).unwrap();
+    HttpResponse::Ok()
+        .cookie(Cookie::build("csrf_token", csrf_token.clone())
+            .path("/")
+            .http_only(false)
+            .secure(true)
+            .same_site(SameSite::Strict)
+            .finish())
+        .json(csrf_token)
+}
+
+pub async fn verify_csrf_token(session: Session, body: web::Json<String>) -> HttpResponse {
+    let token = body.into_inner();
+    
+    let is_valid = match session.get::<String>(CSRF_TOKEN_KEY) {
         Ok(Some(stored_token)) => stored_token == token,
         _ => false,
+    };
+    
+    if is_valid {
+        HttpResponse::Ok().finish()
+    } else {
+        HttpResponse::Forbidden().finish()
     }
-}
-
-pub fn set_csrf_token(session: &Session) -> Option<String> {
-    let csrf_token = generate_csrf_token();
-    match session.insert(CSRF_TOKEN_KEY, csrf_token.clone()) {
-        Ok(_) => Some(csrf_token),
-        Err(_) => None,
-    }
-}
-
-fn extract_csrf_token(req: &HttpRequest) -> Option<String> {
-    req.headers()
-        .get("x-csrf-token")
-        .and_then(|header_value| header_value.to_str().ok().map(|s| s.to_string()))
-}
-
-
-pub fn set_csrf_cookie(csrf_token: &str) -> Cookie<'static> {
-    Cookie::build("csrf_token", csrf_token.to_owned())
-        .path("/")
-        .http_only(false)
-        .secure(true)
-        .same_site(SameSite::Strict)
-        .finish()
 }
