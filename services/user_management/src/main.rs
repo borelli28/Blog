7bb99657343dad0f00fb89939b5fea0cdec1b89f
borelli <borelliarmando28@gmail.com>
2024-09-01 12:00:45 -0400

    Added main.rs

--- services/user_management/src/main.rs
+++ services/user_management/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{Build, Rocket, routes};
+use rocket::fairing::AdHoc;
+use rocket::fs::FileServer;
+use db::AppState;
+mod handlers;
+mod models;
+mod db;
+
+
+#[rocket::launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/user-management", routes![
+            handlers::get_user,
+            handlers::create_user,
+            handlers::update_user,
+            handlers::update_password,
+            handlers::delete_user,
+        ])
+        .mount("/", FileServer::from("static"))
+        .attach(AdHoc::on_ignite("Database", |rocket| async {
+            let app_state = AppState::new().expect("Failed to initialize app state");
+            rocket.manage(app_state)
+        }))
 }
