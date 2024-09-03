3152082d0849afb4c9189caa069a6e341720fa93
borelli <borelliarmando28@gmail.com>
2024-09-03 12:33:15 -0400

    Added auth_middleware.rs

---
+++ services/user_management/src/auth_middleware.rs
@@ -0,0 +1,32 @@
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
+use crate::auth::{validate_token, Claims};
+
+pub struct AuthenticatedUser {
+    pub claims: Claims,
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+    Missing,
+    Invalid,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthenticatedUser {
+    type Error = AuthError;
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let token = request.headers().get_one("Authorization");
+        match token {
+            Some(token) if token.starts_with("Bearer ") => {
+                let token = token.split_at(7).1; // Remove "Bearer " prefix
+                match validate_token(token) {
+                    Ok(claims) => Outcome::Success(AuthenticatedUser { claims }),
+                    Err(_) => Outcome::Error((Status::Unauthorized, AuthError::Invalid)),
+                }
+            }
+            _ => Outcome::Error((Status::Unauthorized, AuthError::Missing)),
+        }
+    }
+}
