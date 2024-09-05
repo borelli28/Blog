c4bd9f5e5cb5b7b020a4a6b6cdb1eaa7d178d7c0
borelli <borelliarmando28@gmail.com>
2024-09-05 09:52:36 -0400

    Added deny list check in validate_token fn

--- services/user_management/src/auth.rs
+++ services/user_management/src/auth.rs
@@ -1,4 +1,5 @@
-use jsonwebtoken::{encode, decode, Header, Validation, EncodingKey, DecodingKey};
+use jsonwebtoken::{encode, decode, Header, Validation, EncodingKey, DecodingKey, errors::Error as JwtError};
+use crate::jwt_deny_list::JwtDenyList;
 use serde::{Serialize, Deserialize};
 use chrono::{Utc, Duration};
 use std::env;
@@ -7,7 +8,7 @@ use std::env;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: u64,  // User ID
-    exp: i64,     // Expiration time
+    pub exp: i64,     // Expiration time
     pub role: String, // User role
 }
 
@@ -26,13 +27,19 @@ pub fn create_token(user_id: &u64, role: &str) -> Result<String, jsonwebtoken::e
     encode(&Header::default(), &claims, &EncodingKey::from_secret(secret_key.as_ref()))
 }
 
-pub fn validate_token(token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
-    let secret_key: String = env::var("JWT_SECRET_KEY").expect("JWT_SECRET_KEY env variable needs to be set");
+pub fn validate_token(token: &str, deny_list: &JwtDenyList) -> Result<Claims, JwtError> {
+    let secret_key = env::var("JWT_SECRET_KEY").expect("JWT_SECRET_KEY env variable needs to be set");
+    // Check if the token is in the deny list
+    match deny_list.is_denied(token) {
+        Ok(true) => return Err(JwtError::from(jsonwebtoken::errors::ErrorKind::InvalidToken)),
+        Ok(false) => {},
+        Err(_) => return Err(JwtError::from(jsonwebtoken::errors::ErrorKind::InvalidToken)),
+    }    
+    // Decode and validate
     let token_data = decode::<Claims>(
         token,
         &DecodingKey::from_secret(secret_key.as_ref()),
         &Validation::default(),
     )?;
-
     Ok(token_data.claims)
 }
