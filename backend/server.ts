98a2e5de727781b3b37bef6e8068ac5781038889
borelli28 <borelliarmando28@gmail.com>
2024-10-31 18:27:57 -0400

    Added credentials in cors policy

--- backend/server.ts
+++ backend/server.ts
@@ -13,7 +13,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173',
+  credentials: true
 }))
 app.use(express.json());
 
