3c9a64d89c57006c9e7547ea69973cfa140d96c8
borelli28 <borelliarmando28@gmail.com>
2024-10-31 18:12:11 -0400

    Added authorization handler for some posts routes

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as postHandlers from '../handlers/posts';
+import { authenticateToken } from '../handlers/auth';
 
 const router = express.Router();
 
 router.get('/', postHandlers.getAllPosts);
 router.get('/:title', postHandlers.getPost);
-router.post('/', postHandlers.createPost);
-router.put('/:title', postHandlers.updatePost);
-router.delete('/', postHandlers.deletePost);
-router.delete('/permanent', postHandlers.permanentDeletePost);
+router.post('/', authenticateToken, postHandlers.createPost);
+router.put('/:title', authenticateToken, postHandlers.updatePost);
+router.delete('/', authenticateToken, postHandlers.deletePost);
+router.delete('/permanent', authenticateToken, postHandlers.permanentDeletePost);
 
 export default router;
