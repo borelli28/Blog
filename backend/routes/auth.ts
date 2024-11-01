a230cd23976f336610956ac4e193febcedd9989b
borelli28 <borelliarmando28@gmail.com>
2024-11-01 07:04:51 -0400

    Added auth check route in backend

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -8,5 +8,8 @@ router.post('/register', authHandlers.register);
 router.post('/login', authHandlers.login);
 router.post('/logout', authHandlers.logout);
 router.put('/password', authenticateToken, authHandlers.updatePassword);
+router.get('/check', authenticateToken, (req, res) => {
+  res.status(200).json({ authenticated: true });
+});
 
 export default router;
