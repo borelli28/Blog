b8eba37a7e23ab3c1892823e8385e488e17e55ac
borelli28 <borelliarmando28@gmail.com>
2024-11-04 13:03:02 -0500

    Added updatePostStatus handler method in backend to handle favorite and Publish status changes for posts

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -8,6 +8,7 @@ router.get('/', postHandlers.getAllPosts);
 router.get('/:title', postHandlers.getPost);
 router.post('/', authenticateToken, postHandlers.createPost);
 router.put('/:title', authenticateToken, postHandlers.updatePost);
+router.patch('/:title/status', authenticateToken, postHandlers.updatePostStatus);
 router.delete('/', authenticateToken, postHandlers.deletePost);
 router.delete('/permanent', authenticateToken, postHandlers.permanentDeletePost);
 router.get('/:title/images', postHandlers.getPostImages);
