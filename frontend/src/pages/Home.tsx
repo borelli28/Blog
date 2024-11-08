19e2655112080c6a71515e0f05ee96b03a04911c
borelli28 <borelliarmando28@gmail.com>
2024-11-08 12:24:39 -0500

    Added stylesheet for PostList page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
+import '../styles/Home.css';
 
 interface Blog {
   title: string;
