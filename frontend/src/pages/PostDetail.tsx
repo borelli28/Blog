2104a0371f532f8485abdf76e5d94df117c69a40
borelli28 <borelliarmando28@gmail.com>
2024-11-09 05:43:19 -0500

    Added Blog stylesheet

--- frontend/src/pages/PostDetail.tsx
+++ frontend/src/pages/PostDetail.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Layout from '../components/Layout';
+import '../styles/Blog.css';
 
 interface Blog {
   title: string;
