1c8219b5d11fc543b52c1ddc0ebfdc34b886515d
borelli28 <borelliarmando28@gmail.com>
2024-11-09 22:20:56 -0500

    Fixed posts routes file, missing semicolon

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -12,7 +12,7 @@ router.delete('/permanent', authenticateToken, postHandlers.permanentDeletePost)
 router.get('/:title', postHandlers.getPost);
 router.post('/', authenticateToken, postHandlers.createPost);
 router.put('/:title', authenticateToken, postHandlers.updatePost);
-router.patch('/:title/recover', authenticateToken, postHandlers.recoverPost)
+router.patch('/:title/recover', authenticateToken, postHandlers.recoverPost);
 router.patch('/:title/status', authenticateToken, postHandlers.updatePostStatus);
 router.delete('/:title', authenticateToken, postHandlers.deletePost);
 router.get('/:title/images', postHandlers.getPostImages);
