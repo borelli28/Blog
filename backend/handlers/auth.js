935278bd1351e58a36006bafad2124fde89bae1d
borelli28 <borelliarmando28@gmail.com>
2024-12-13 05:57:43 -0500

    Fixed import of middleware/auth.js in handlers/auth.js

--- backend/handlers/auth.js
+++ backend/handlers/auth.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 import logger from '../utils/logger.js';
 import { addToken, removeToken, isTokenValid } from '../utils/tokenWhitelist.js';
 import { getUsernameFromToken } from '../utils/getUsernameFromToken.js';
-import { refreshToken, authenticateToken, refreshToken } from '../middleware/auth.js';
+import { authenticateToken, refreshToken } from '../middleware/auth.js';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
