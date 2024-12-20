8a65470f028fffe0520b9654b7b6c493d143bf68
borelli28 <borelliarmando28@gmail.com>
2024-12-20 12:51:57 -0500

    Added validator check to only allow 1 user in the app: 'admin'

--- backend/utils/validators.js
+++ backend/utils/validators.js
@@ -11,6 +11,8 @@ export const userRegisterValidator = (postData) => {
 
   if (postData.username.length < 3) {
     errors.username = "Username should be at least 3 characters long";
+  } else if (postData.username !== "admin") {
+    errors.username = "Username is not valid";
   } else if (postData.username.length > 30) {
     errors.username = "Username cannot be longer than 30 characters";
   } else if (!/^[a-zA-Z0-9_]+$/.test(postData.username)) {
@@ -30,7 +32,7 @@ export const userRegisterValidator = (postData) => {
   return new Promise((resolve) => {
     db.get('SELECT * FROM users WHERE username = ?', [postData.username], (err, row) => {
       if (row) {
-        errors.username = "Username is not valid. Please enter a valid username";
+        errors.username = "Username is not valid";
       }
       resolve(errors);
     });
