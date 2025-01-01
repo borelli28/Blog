6ea8e35af0338ac3244abe7c7342ffe75c43f88d
borelli28 <borelliarmando28@gmail.com>
2025-01-01 07:51:31 -0500

    Added route to remove multiple logs by filter

--- backend/routes/logs.js
+++ backend/routes/logs.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/', authenticateToken, logHandlers.getLogs);
 router.delete('/:timestamp', authenticateToken, logHandlers.removeLog); 
+router.delete('/filtered', authenticateToken, logHandlers.removeFilteredLogs);
 
 export default router;
