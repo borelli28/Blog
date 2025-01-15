ccaacd48322afb103a3094d9bfe587627e67ba74
borelli <borelliarmando28@gmail.com>
2025-01-15 00:06:10 -0500

    Added lazy load for all my pages to reduce initial loading time of frontend app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,63 @@
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
-import Home from './pages/Home';
-import PostList from './pages/PostList';
-import PostDetail from './pages/PostDetail';
-import CreatePost from './pages/CreatePost';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
-import EditPost from './pages/EditPost';
-import Logs from './pages/Logs';
 import Logout from './components/Logout';
+
+const Home = lazy(() => import('./pages/Home'));
+const PostList = lazy(() => import('./pages/PostList'));
+const PostDetail = lazy(() => import('./pages/PostDetail'));
+const CreatePost = lazy(() => import('./pages/CreatePost'));
+const Login = lazy(() => import('./pages/Login'));
+const Register = lazy(() => import('./pages/Register'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const EditPost = lazy(() => import('./pages/EditPost'));
+const Logs = lazy(() => import('./pages/Logs'));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<PostList />} />
-        <Route path="/blog/:id" element={<PostDetail />} />
-        <Route path="/create" element={<ProtectedRoute><CreatePost /></ProtectedRoute>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/blog/edit/:id" element={<ProtectedRoute><EditPost /></ProtectedRoute>} />
-        <Route path="/logs" element={<ProtectedRoute><Logs /></ProtectedRoute>} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<PostList />} />
+          <Route path="/blog/:id" element={<PostDetail />} />
+          <Route
+            path="/create"
+            element={
+              <ProtectedRoute>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/blog/edit/:id"
+            element={
+              <ProtectedRoute>
+                <EditPost />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/logs"
+            element={
+              <ProtectedRoute>
+                <Logs />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
